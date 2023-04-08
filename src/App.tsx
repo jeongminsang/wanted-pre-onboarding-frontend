b6c4e522f0fc2e8d52786a594e29de4e60d51963
jeongminsang <101001956+jeongminsang@users.noreply.github.com>
2023-04-08 23:43:48 +0900

    feat: 리다이렉트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Main from "./pages/Main";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Header from "./Header";
 
 function App() {
+  // console.log(localStorage.token);
   return (
     <div className="App">
       <Header/>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/todo" element={localStorage.token !== undefined ? <Main /> : <Navigate to="/signin"/>}/>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
       </Routes>
